d1bb893b12d6df520f591060e7cca0dfd897ebcf
JasrajSingh786 <97440372+JasrajSingh786@users.noreply.github.com>
2022-01-10 16:09:54 +0530

    Add files via upload

---
+++ OutputFile.py
@@ -0,0 +1,36 @@
+Python 3.10.1 (tags/v3.10.1:2cd268a, Dec  6 2021, 19:10:37) [MSC v.1929 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+
+= RESTART: C:\Users\Jaideep kaur\Desktop\Goshi's Folder\Python programs for Assignment 1\QuestionNo.1.py
+Enter the value of a: 30
+Enter the value of b: 40
+Enter the value of c: 50
+The value of the average of a, b and c is: 40.0
+
+= RESTART: C:\Users\Jaideep kaur\Desktop\Goshi's Folder\Python programs for Assignment 1\QuestionNo.2.py
+Enter the value of Gross income: 40000
+Enter the value of Number of dependents: 6
+The value of income tax is:  12000
+
+= RESTART: C:\Users\Jaideep kaur\Desktop\Goshi's Folder\Python programs for Assignment 1\QuestionNo.3.py
+Enter the SID: 21103106
+Enter the Name: Jasraj Singh Sohanpal
+Enter the Gender: M
+Enter the name of the Course: CSE
+Enter the value of CGPA: 9.8
+[21103106, 'Jasraj Singh Sohanpal', 'M', 'CSE', 9.8]
+
+= RESTART: C:\Users\Jaideep kaur\Desktop\Goshi's Folder\Python programs for Assignment 1\QuestionNo.4.py
+The marks of Student 1 is: 99
+The marks of Student 2 is: 78
+The marks of Student 3 is: 89
+The marks of Student 4 is: 56
+The marks of Student 5 is: 94
+['56', '78', '89', '94', '99']
+
+= RESTART: C:\Users\Jaideep kaur\Desktop\Goshi's Folder\Python programs for Assignment 1\QuestionNo.5a.py
+ The element to be removed is: Black
+['Red', 'Green', 'White', 'Pink', 'Yellow']
+
+= RESTART: C:\Users\Jaideep kaur\Desktop\Goshi's Folder\Python programs for Assignment 1\QuestionNo.5b.py
+['Red', 'Green', 'White', 'Purple', 'Yellow']
