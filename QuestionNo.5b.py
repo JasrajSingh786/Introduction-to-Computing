853fcb3a189ac7367a31106c55cbf6dcb4293105
JasrajSingh786 <97440372+JasrajSingh786@users.noreply.github.com>
2022-01-21 19:05:59 +0530

    Delete QuestionNo.5b.py

--- QuestionNo.5b.py
+++
@@ -1,4 +0,0 @@
-#To replace two elements in a list and replace that with a single element.
-colours = ['Red', 'Green', 'White', 'Black', 'Pink', 'Yellow']
-colours[3:5] = ['Purple'] #Because the index of Black is 3 and [3:5] is interpreted as replace from Index 3 to Index 4 by 'Purple'. 
-print(colours)
