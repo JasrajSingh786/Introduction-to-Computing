ebda62ebe2364c7587a7443491dd21a0eac46567
JasrajSingh786 <97440372+JasrajSingh786@users.noreply.github.com>
2022-01-21 18:59:03 +0530

    Add files via upload

---
+++ Assignment 2/OutputFile.py
@@ -0,0 +1,40 @@
+Python 3.10.1 (tags/v3.10.1:2cd268a, Dec  6 2021, 19:10:37) [MSC v.1929 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+
+= RESTART: C:\Users\Jaideep kaur\Desktop\Python programs for Assignment 2\QuestionNo.1.py
+The length of Input String is: 35
+The reverse of Input String is: egaugnal evitisnes esac a si nohtyP
+a case sensitive
+The updated string will be: Python is object orientated language
+The Index of Substring 'a' is: 10
+Pythonisacasesensitivelanguage
+
+= RESTART: C:\Users\Jaideep kaur\Desktop\Python programs for Assignment 2\QuestionNo.2.py
+Hey, Jasraj Singh Sohanpal Here!
+My SID is 21103106.
+I am from Computer Science department and My CGPA is 9.9.
+
+= RESTART: C:\Users\Jaideep kaur\Desktop\Python programs for Assignment 2\QuestionNo.3.py
+8
+58
+50
+224
+40
+14
+0
+
+= RESTART: C:\Users\Jaideep kaur\Desktop\Python programs for Assignment 2\QuestionNo.4.py
+Enter the value of number 1: 34
+Enter the value of number 2: 56
+Enter the value of number 3: 78
+Greatest number out of the three is: 78.0
+
+= RESTART: C:\Users\Jaideep kaur\Desktop\Python programs for Assignment 2\QuestionNo.5.py
+Enter the string: My name is Jasraj Singh Sohanpal
+Yes
+
+= RESTART: C:\Users\Jaideep kaur\Desktop\Python programs for Assignment 2\QuestionNo.6.py
+Enter the length of first side of triangle: 3.6
+Enter the length of second side of triangle: 4.7
+Enter the length of third side of triangle: 5.4
+Yes, A Triangle can be formed
