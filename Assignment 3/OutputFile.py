64b7a480a7a8f4275f321e7d0f901f4e7a2dd919
JasrajSingh786 <97440372+JasrajSingh786@users.noreply.github.com>
2022-02-09 19:20:17 +0530

    Add files via upload

---
+++ Assignment 3/OutputFile.py
@@ -0,0 +1,84 @@
+Python 3.10.1 (tags/v3.10.1:2cd268a, Dec  6 2021, 19:10:37) [MSC v.1929 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+
+= RESTART: C:\Users\Jaideep kaur\Desktop\Jasraj's Folder\Python\Python programs for Assignment 3\Question.No.1.py
+Enter the Input String: Jasraj
+{'J': 1, 'a': 2, 's': 1, 'r': 1, 'j': 1}
+
+= RESTART: C:\Users\Jaideep kaur\Desktop\Jasraj's Folder\Python\Python programs for Assignment 3\Question.No.2.py
+ENTER THE DATE: 28
+ENTER THE MONTH: 2
+ENTER THE YEAR BETWEEN 1800 to 2025 INCLUDING BOTH THE END YEAR'S: 1807
+The Next Date is: 1-3-1807
+
+= RESTART: C:\Users\Jaideep kaur\Desktop\Jasraj's Folder\Python\Python programs for Assignment 3\Question.No.3.py
+Enter the First number: 3
+Do you want to add more: (Yes or No); Yes
+Enter the Number: 5
+Do you want to add more: (Yes or No); Yes
+Enter the Number: 7
+Do you want to add more: (Yes or No); Yes
+Enter the Number: 9
+Do you want to add more: (Yes or No); No
+The Input List is: [3, 5, 7, 9]
+The Output List is:  [(3, 9), (5, 25), (7, 49), (9, 81)]
+
+= RESTART: C:\Users\Jaideep kaur\Desktop\Jasraj's Folder\Python\Python programs for Assignment 3\Question.No.4.py
+Enter your Grade point: 3
+ValueError: Grade point out of Range 
+Please Re-enter the values:
+Enter your Grade point: 10
+Your Grade is 'A+' and Outstanding performance 
+
+= RESTART: C:\Users\Jaideep kaur\Desktop\Jasraj's Folder\Python\Python programs for Assignment 3\Question.No.5.py
+The Required Pattern is: 
+ABCDEFGHIJK
+ ABCDEFGHI
+  ABCDEFG
+   ABCDE
+    ABC
+     A
+
+= RESTART: C:\Users\Jaideep kaur\Desktop\Jasraj's Folder\Python\Python programs for Assignment 3\Question.No.6.py
+Enter the name of Student: Jasraj Singh 
+Enter the SID of Student: 21103106
+Do you want to add another student Yes,Y or No,N: Y
+Enter the name of Student: Yuvraj Singh
+Enter the SID of Student: 21103049
+Do you want to add another student Yes,Y or No,N: Y
+Enter the name of Student: Sukhman Singh
+Enter the SID of Student: 21103060
+Do you want to add another student Yes,Y or No,N: Y
+Enter the name of Student: Ishita Gera 
+Enter the SID of Student: 21103094
+Do you want to add another student Yes,Y or No,N: N
+a) The list of Students is as follows: 
+ {21103106: 'Jasraj Singh ', 21103049: 'Yuvraj Singh', 21103060: 'Sukhman Singh', 21103094: 'Ishita Gera '}
+b) Sorted Dictionary according to their alphabetical order of their names is: 
+ {21103094: 'Ishita Gera ', 21103106: 'Jasraj Singh ', 21103060: 'Sukhman Singh', 21103049: 'Yuvraj Singh'}
+c) Sorted Dictionary according to the ascending order of SID's is: 
+ {21103049: 'Yuvraj Singh', 21103060: 'Sukhman Singh', 21103094: 'Ishita Gera ', 21103106: 'Jasraj Singh '}
+d) Enter the SID whose Name you want: 21103060
+The Name of the Student with SID as 21103060 is:  Sukhman Singh
+
+========================= RESTART: C:\Users\Jaideep kaur\Desktop\Jasraj's Folder\Python\Python programs for Assignment 3\Question.No.7.py =========================
+The Number of terms of Fibonacci Sequence which are to be listed: 10
+Fibonacci Sequence of first '10' terms:
+1
+1
+2
+3
+5
+8
+13
+21
+34
+55
+The Average of First '10' terms of Fibonacci Sequence is:  14.3
+
+========================= RESTART: C:\Users\Jaideep kaur\Desktop\Jasraj's Folder\Python\Python programs for Assignment 3\Question.No.8.py =========================
+a) Set of all elements that are in Set1 and Set2 but not both:  {1, 3, 5, 6, 8}
+b) Set of all elements that are in only one of the three sets Set1, Set2 and Set3:  {17, 3, 6, 8, 9, 13}
+c) Set of elements that are exactly two of the sets Set1, Set2 and Set3:  {1, 2, 4, 5}
+d) Set of all integers in the range 1 to 10 that are not in Set1:  {6, 7, 8, 9, 10}
+e) Set of all integers in the range 1 to 10 that are not in Set1,Set2 and Set3:  {10, 7}
