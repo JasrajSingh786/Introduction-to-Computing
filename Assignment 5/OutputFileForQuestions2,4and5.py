833d2e64f043625b03a4f7ee75f196160430456f
JasrajSingh786 <97440372+JasrajSingh786@users.noreply.github.com>
2022-03-07 22:43:54 +0530

    Add files via upload

---
+++ Assignment 5/OutputFileForQuestions2,4and5.py
@@ -0,0 +1,83 @@
+Python 3.10.1 (tags/v3.10.1:2cd268a, Dec  6 2021, 19:10:37) [MSC v.1929 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+
+= RESTART: E:\Desktop data\Jasraj's Folder\Python\Python programs for Assignment 5\Question.No.2.py
+The calender is shown below:
+                                  2022
+
+      January                   February                   March
+Mo Tu We Th Fr Sa Su      Mo Tu We Th Fr Sa Su      Mo Tu We Th Fr Sa Su
+                1  2          1  2  3  4  5  6          1  2  3  4  5  6
+ 3  4  5  6  7  8  9       7  8  9 10 11 12 13       7  8  9 10 11 12 13
+10 11 12 13 14 15 16      14 15 16 17 18 19 20      14 15 16 17 18 19 20
+17 18 19 20 21 22 23      21 22 23 24 25 26 27      21 22 23 24 25 26 27
+24 25 26 27 28 29 30      28                        28 29 30 31
+31
+
+       April                      May                       June
+Mo Tu We Th Fr Sa Su      Mo Tu We Th Fr Sa Su      Mo Tu We Th Fr Sa Su
+             1  2  3                         1             1  2  3  4  5
+ 4  5  6  7  8  9 10       2  3  4  5  6  7  8       6  7  8  9 10 11 12
+11 12 13 14 15 16 17       9 10 11 12 13 14 15      13 14 15 16 17 18 19
+18 19 20 21 22 23 24      16 17 18 19 20 21 22      20 21 22 23 24 25 26
+25 26 27 28 29 30         23 24 25 26 27 28 29      27 28 29 30
+                          30 31
+
+        July                     August                  September
+Mo Tu We Th Fr Sa Su      Mo Tu We Th Fr Sa Su      Mo Tu We Th Fr Sa Su
+             1  2  3       1  2  3  4  5  6  7                1  2  3  4
+ 4  5  6  7  8  9 10       8  9 10 11 12 13 14       5  6  7  8  9 10 11
+11 12 13 14 15 16 17      15 16 17 18 19 20 21      12 13 14 15 16 17 18
+18 19 20 21 22 23 24      22 23 24 25 26 27 28      19 20 21 22 23 24 25
+25 26 27 28 29 30 31      29 30 31                  26 27 28 29 30
+
+      October                   November                  December
+Mo Tu We Th Fr Sa Su      Mo Tu We Th Fr Sa Su      Mo Tu We Th Fr Sa Su
+                1  2          1  2  3  4  5  6                1  2  3  4
+ 3  4  5  6  7  8  9       7  8  9 10 11 12 13       5  6  7  8  9 10 11
+10 11 12 13 14 15 16      14 15 16 17 18 19 20      12 13 14 15 16 17 18
+17 18 19 20 21 22 23      21 22 23 24 25 26 27      19 20 21 22 23 24 25
+24 25 26 27 28 29 30      28 29 30                  26 27 28 29 30 31
+31
+
+
+= RESTART: E:\Desktop data\Jasraj's Folder\Python\Python programs for Assignment 5\Question.No.4.py
+Enter the Number of Students: 5
+Enter the Marks of First Student: 12
+Enter the Marks of Next Student: 2
+Enter the Marks of Next Student: 3
+Enter the Marks of Next Student: 34
+Enter the Marks of Next Student: 1
+The Input Array of Marks of 5 Students is:  [12, 2, 3, 34, 1]
+The Sorted Array of Marks of 5 Students is:  [1, 2, 3, 12, 34]
+
+================================ RESTART: E:\Desktop data\Jasraj's Folder\Python\Python programs for Assignment 5\Question.No.5.py ================================
+Enter the First Number: 23
+Do yo want to add more (Yes or No): Yes
+Enter the Number: 34
+Do yo want to add more (Yes or No): Yes
+Enter the Number: 67
+Do yo want to add more (Yes or No): Yes
+Enter the Number: 56
+Do yo want to add more (Yes or No): Yes
+Enter the Number: 67
+Do yo want to add more (Yes or No): Yes
+Enter the Number: 23
+Do yo want to add more (Yes or No): Yes
+Enter the Number: 45
+Do yo want to add more (Yes or No): Yes
+Enter the Number: 34
+Do yo want to add more (Yes or No): Yes
+Enter the Number: 78
+Do yo want to add more (Yes or No): Yes
+Enter the Number: 89
+Do yo want to add more (Yes or No): Yes
+Enter the Number: 90
+Do yo want to add more (Yes or No): Yes
+Enter the Number: 100
+Do yo want to add more (Yes or No): No
+The Input Array is:  [23, 34, 67, 56, 67, 23, 45, 34, 78, 89, 90, 100]
+The Sorted Input List:  [23, 23, 34, 34, 45, 56, 67, 67, 78, 89, 90, 100]
+Enter the Number which you want to search in the Array: 67
+The Index of the required element is:  6
+The Number of Occurences of Element is:  2
